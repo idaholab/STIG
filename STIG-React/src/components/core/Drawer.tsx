4e0113674cd1b26ae38bbc51db6485e3271ee06e
isaiah-mocombe <isaiah.mocombe@inl.gov>
2024-07-16 15:25:17 -0600

    dropdown menus

--- STIG-React/src/components/core/Drawer.tsx
+++ STIG-React/src/components/core/Drawer.tsx
@@ -1,9 +1,11 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks/reduxTypescriptHooks';
 import { appStateActions } from '../../../app/store/index';
+import FilterBar from './filterBar';
 // import { AppReport } from '../../../app/types/types';
 
 import { Link } from 'react-router-dom';
+import SDODropdown from './SdoDropdown';
 
 interface OpenSections {
   [key: string]: boolean;
@@ -43,7 +45,7 @@ const AccordionSection: React.FC<AccordionSectionProps> = ({ title, isOpen, togg
           </span>
         </span>
         <button onClick={handleAddClick} className="btn btn-ghost btn-circle btn-sm">
-          <span className="material-icons" style={{fontSize: '1.4rem'}}>add</span>
+          <span className="material-icons" style={{ fontSize: '1.4rem' }}>add</span>
         </button>
       </div>
       <div
@@ -72,6 +74,10 @@ const Drawer = () => {
     return isRootActive ? 'bg-gray-300 dark:bg-gray-700' : '';
   };
 
+  function handleFilterChange(filterText: string): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <>
       <aside className="p-1 bg-slate-300 dark:bg-gray-950 text-base-content w-80" style={{ height: 'calc(100vh - 64px)' }}>
@@ -79,12 +85,24 @@ const Drawer = () => {
           <div>
             <ul className="menu text-gray-800 dark:text-gray-200">
               <li>
+                {/*<FilterBar onFilterChange={handleFilterChange} />  Pass the handler */}
+                {/* Other menu items */}
                 <Link to={'/'} className={`px-2 !gap-0 ${isActive('/')}`}>
                   <span className="material-icons mr-2">dashboard</span>
                   <span className={`whitespace-nowrap overflow-hidden`}>
                     Getting Started
                   </span>
                 </Link>
+              </li>
+              <li>
+                <AccordionSection title="SDOs" isOpen={false} toggleAccordion={() => { }}>
+                  <SDODropdown />
+                </AccordionSection>
+              </li>
+              <li>
+                <AccordionSection title="SCOs" isOpen={false} toggleAccordion={() => { }}>
+                  <SDODropdown />
+                </AccordionSection>
               </li>
             </ul>
           </div>
