bfe27d65faffaadaa4bb9a5cf769587dc38701da
isaiah-mocombe <isaiah.mocombe@inl.gov>
2024-08-01 12:03:17 -0600

    drawer file

--- STIG-React/src/components/core/Drawer.tsx
+++ STIG-React/src/components/core/Drawer.tsx
@@ -1,14 +1,23 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import StencilLibrary from '../Graphs/StencilLibrary';
-import AccordionSection from './AccordionSection';
+import React, { useState, useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks/reduxTypescriptHooks';
 import { EventContext } from '@/contexts/EventContext';
+import AccordionSection from './AccordionSection';
+import StencilLibrary from '../Graphs/StencilLibrary';
 
 const Drawer = () => {
+  const dispatch = useAppDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const storeReportsSharedList = useAppSelector((state: any) => state.appState.reportsSharedList);
+  const storeCommonLinksList = useAppSelector((state: any) => state.appState.commonLinksList);
+  const selectedReportIndex = useAppSelector((state: any) => state.appState.selectedReportIndex);
+
   const [openSections, setOpenSections] = useState<{ [key: string]: boolean }>({
     sdo: false,
     sco: false,
   });
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const toggleAccordion = (section: string) => {
     setOpenSections(prevState => ({
@@ -17,62 +26,79 @@ const Drawer = () => {
     }));
   };
 
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   const { dispatchEvent } = useContext(EventContext);
-  const handleAddStancilNode = (name: string, imageUrl: string) => {
+  const handleAddStencilNode = (name: string, imageUrl: string) => {
     const customEvent = new CustomEvent('addNode', {
       detail: {
         label: name,
         imageUrl,
-        position: { x: 200, y: 200 }, // TODO: modify this to set a specific position. 
+        position: { x: 200, y: 200 }, // TODO: modify this to set a specific position.
       },
     });
-    // Add the node to the graph through context event disbatch
     dispatchEvent('stencilMouseUpEvent', { data: customEvent });
   };
 
   const isActive = (path: string) => {
     const currentPath = location.pathname;
-    const isRootActive = path === '/' && currentPath === '/';
-    const isExactMatch = currentPath === path;
-    return isRootActive ? 'bg-gray-300 dark:bg-gray-700' : '';
+    return currentPath === path ? 'bg-gray-300 dark:bg-gray-700' : '';
   };
 
-  function handleFilterChange(filterText: string): void {
-    throw new Error('Function not implemented.');
-  }
-
   return (
-    <>
-      <aside className="flex flex-col bg-gray-300 dark:bg-gray-950 text-base-content w-80 overflow-hidden" style={{ height: 'calc(100vh - 64px)' }}>
-        <div className="flex flex-col justify-between h-full overflow-hidden">
-          <div className="flex-grow scrollbar">
-            <ul className="menu text-gray-800 dark:text-gray-200 w-80 ">
-              <li>
-                {/*<FilterBar onFilterChange={handleFilterChange} />  Pass the handler */}
-                {/* Other menu items */}
-                <Link to={'/'} className={`px-2 !gap-0 ${isActive('/')}`}>
-                  <span className="material-icons mr-2">dashboard</span>
-                  <span className={`whitespace-nowrap overflow-hidden`}>
-                    Getting Started
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <AccordionSection title="Stix Domain Objects (SDO)" isOpen={openSections['sdo']} toggleAccordion={() => toggleAccordion('sdo')}>
-                  <StencilLibrary type="sdo" onAddNode={handleAddStancilNode} />
-                </AccordionSection>
-              </li>
-              <li>
-                <AccordionSection title="Stix Cyber-Observable Objects (SCO)" isOpen={openSections['sco']} toggleAccordion={() => toggleAccordion('sco')}>
-                  <StencilLibrary type="sco" onAddNode={handleAddStancilNode} />
-                </AccordionSection>
-              </li>
-            </ul>
-          </div>
-          <span className="copyright-box m-4">&copy;{new Date().getFullYear()} Idaho National Laboratory</span>
+    <aside className={`flex flex-col bg-gray-300 dark:bg-gray-950 text-base-content ${isCollapsed ? 'w-20' : 'w-80'} transition-all`} style={{ height: 'calc(100vh - 64px)' }}>
+      <div className="flex flex-col justify-between h-full">
+        <div className="flex-grow overflow-auto grid scrollbar">
+          <ul className="menu text-gray-800 dark:text-gray-200">
+            <li>
+              <button
+                onClick={toggleCollapse}
+                className="flex items-center justify-center w-[40px] h-[40px] rounded-lg my-1 hover:bg-gray-200 dark:hover:bg-gray-700 transition"
+              >
+                <span className="material-icons">
+                  {isCollapsed ? 'menu' : 'menu_open'}
+                </span>
+              </button>
+            </li>
+            <li>
+              <Link
+                to={'/'}
+                className={`px-2 h-[48px] flex items-center hover:bg-gray-200 dark:hover:bg-gray-700 ${isActive('/')}`}>
+                <span className="material-icons mr-2">
+                  dashboard
+                </span>
+                {!isCollapsed && <span className="whitespace-nowrap overflow-hidden">Getting Started</span>}
+              </Link>
+            </li>
+            <li>
+              <AccordionSection
+                title="Stix Domain Objects (SDO)"
+                isOpen={openSections['sdo']}
+                toggleAccordion={() => toggleAccordion('sdo')}
+              >
+                <StencilLibrary type="sdo" onAddNode={handleAddStencilNode} />
+              </AccordionSection>
+            </li>
+            <li>
+              <AccordionSection
+                title="Stix Cyber-Observable Objects (SCO)"
+                isOpen={openSections['sco']}
+                toggleAccordion={() => toggleAccordion('sco')}
+              >
+                <StencilLibrary type="sco" onAddNode={handleAddStencilNode} />
+              </AccordionSection>
+            </li>
+          </ul>
         </div>
-      </aside>
-    </>
+        {!isCollapsed && (
+          <div className="m-4">
+            <span className="copyright-box">&copy;{new Date().getFullYear()} Idaho National Laboratory</span>
+          </div>
+        )}
+      </div>
+    </aside>
   );
 };
 
