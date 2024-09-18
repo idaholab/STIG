012d57163727dc2e3372d37b6b26914d274ec8f1
paultd <tony.paul@inl.gov>
2024-09-18 09:43:37 -0600

    WIP

--- STIG-React/src/types/SchemaType.ts
+++ STIG-React/src/types/SchemaType.ts
@@ -14,7 +14,8 @@ export type SchemaType =
 export type s_SchemaType =
   "Binary" | "Boolean" | "Dictionary" | "external_reference" | "Float" | "Hashes" | "Hex" |
   "Identifier" | "Integer" | "kill_chain_phase" | "String" | "Timestamp" | "open_vocab" | "Enum" |
-  "List" | "granular_marking" | "email_mime_part_type" | "windows_registry_value_type" | "x509_v3_extensions_type" | 'EmbeddedMap' | 'EmbeddedList';
+  "List" | "granular_marking" | "email_mime_part_type" | "windows_registry_value_type" | "x509_v3_extensions_type";
+//| 'EmbeddedMap' | 'EmbeddedList'
 
 export type Binary = string;
 export type Boolean = boolean;
