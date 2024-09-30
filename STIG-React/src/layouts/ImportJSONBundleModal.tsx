d131a34ccd7ad3abc82a9b9cff4b6b13405c1f32
paultd <tony.paul@inl.gov>
2024-09-30 06:18:15 -0600

    Not a fan of git's inability to merge!

--- STIG-React/src/layouts/ImportJSONBundleModal.tsx
+++ STIG-React/src/layouts/ImportJSONBundleModal.tsx
@@ -26,24 +26,20 @@ const ImportJSONBundleModal: React.FC = () => {
                 additionalClasses={'place-self-end mt-8' + (!selectedFile ? " btn-disabled" : "")}
                 onClick={() => {
                     if (cyInstance) {
-                        const [numVertificesAdded, numEdgesAdded] = addToGraph(JSON.parse(selectedFile as string), cyInstance);
-                        if (numVertificesAdded < 0 && numEdgesAdded < 0) {
-                            if (cyInstance) {
-                                const [numVerticiesAdded, numEdgesAdded] = addToGraph(JSON.parse(selectedFile as string), cyInstance);
-                                if (numVerticiesAdded < 0 && numEdgesAdded < 0) {
-                                    addNotification("Import failed", "error");
-                                } else if (numVerticiesAdded === 0 && numEdgesAdded === 0) {
-                                    addNotification("Imported " + numVerticiesAdded + " node(s) and " + numEdgesAdded + " edge(s)", "warning");
-                                } else {
-                                    addNotification("Imported " + numVerticiesAdded + " node(s) and " + numEdgesAdded + " edge(s)", "success");
-                                }
-                            }
-                            // Close the dialog
-                            const dialogElement = document.getElementById("ImportJSONBundleModal") as HTMLDialogElement;
-                            dialogElement.close();
+                        const [numVerticiesAdded, numEdgesAdded] = addToGraph(JSON.parse(selectedFile as string), cyInstance);
+                        if (numVerticiesAdded < 0 && numEdgesAdded < 0) {
+                            addNotification("Import failed", "error");
+                        } else if (numVerticiesAdded === 0 && numEdgesAdded === 0) {
+                            addNotification("Imported " + numVerticiesAdded + " node(s) and " + numEdgesAdded + " edge(s)", "warning");
+                        } else {
+                            addNotification("Imported " + numVerticiesAdded + " node(s) and " + numEdgesAdded + " edge(s)", "success");
                         }
                     }
-                }}
+                    // Close the dialog
+                    const dialogElement = document.getElementById("ImportJSONBundleModal") as HTMLDialogElement;
+                    dialogElement.close();
+                }
+                }
             />
         </div>
     );
