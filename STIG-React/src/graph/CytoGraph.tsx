a64cd5b671048cc2c704e3f8faa7c25fe798733f
Logan Kearsley <logan.kearsley@inl.gov>
2024-11-19 16:37:19 -0700

    Don't unselect stuff when adding a new node

--- STIG-React/src/graph/CytoGraph.tsx
+++ STIG-React/src/graph/CytoGraph.tsx
@@ -291,7 +291,6 @@ const Graph: React.FC = () => {
     // Auto Select the Added Node if nothing else is already selected.
     function autoSelectNewNode(cy: cytoscape.Core, addedNode: SingularElementReturnValue) {
         if (cy) {
-            cy.$(':selected').unselect();
             addedNode.select();
             setSelectedSTIXObject(addedNode.data("raw_data"));
             setSelectionExists(true);
