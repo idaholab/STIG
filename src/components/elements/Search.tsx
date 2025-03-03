814af3d45e53e22ad656b856b8c214a3af711a03
william-brant <william.brant@inl.gov>
2025-03-03 10:55:11 -0600

    fixed bad count in search/find functionality

--- src/components/elements/Search.tsx
+++ src/components/elements/Search.tsx
@@ -125,7 +125,9 @@ const SearchComponent: React.FC = () => {
         });
 
         if (eles.length) {
+            let eleCount = 0; //count up total matching elements for each property that was searched
             eles.forEach((ele) => {
+                eleCount += ele.length;
                 if (ele.isEdge()) {
                     selected = selected.add(ele.sources());
                     selected = selected.add(ele.targets());
@@ -134,7 +136,7 @@ const SearchComponent: React.FC = () => {
             });
             selected.select();
 
-            setSearchStatus(`Found ${eles.length} element${eles.length !== 1 ? 's' : ''}`);
+            setSearchStatus(`Found ${eleCount} element${eleCount !== 1 ? 's' : ''}`);
             cyInstance.animate({
                 fit: {
                     eles: cyInstance.elements(),
