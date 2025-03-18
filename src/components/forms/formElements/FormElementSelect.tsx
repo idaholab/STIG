31d85b2202bccfc201460133c0496b406080b7ee
paultd <tony.paul@inl.gov>
2025-03-18 15:03:45 -0600

    I believe 308 is addressed with the useEffect updates on FormElementSTIXDictionary

--- src/components/forms/formElements/FormElementSelect.tsx
+++ src/components/forms/formElements/FormElementSelect.tsx
@@ -1,9 +1,9 @@
 // React
-import AlertComponent from '@/components/elements/AlertComponent';
-import InfoButton from '@/components/elements/InfoButton';
-import STIXPropertyLabel from '@/components/elements/STIXPropertyLabel';
-import { SchemaSTIXProperty } from '@/types/stixSchemaTypes/SchemaSTIXProperty';
-import React, { useRef, useState } from 'react';
+import AlertComponent from "@/components/elements/AlertComponent";
+import InfoButton from "@/components/elements/InfoButton";
+import STIXPropertyLabel from "@/components/elements/STIXPropertyLabel";
+import { SchemaSTIXProperty } from "@/types/stixSchemaTypes/SchemaSTIXProperty";
+import React, { useRef, useState } from "react";
 
 type Props = {
   label?: string;
@@ -15,12 +15,12 @@ type Props = {
   disabled?: boolean;
   includeInfo?: boolean;
   infoText?: string;
-  infoIcon?: string,
+  infoIcon?: string;
   additionalClasses?: string;
   additionalInfoClasses?: string;
-  property?: SchemaSTIXProperty,
-  showTypeSelector?: boolean,
-  onTypeChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void,
+  property?: SchemaSTIXProperty;
+  showTypeSelector?: boolean;
+  onTypeChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
 const FormElementSelect: React.FC<Props> = ({
@@ -29,7 +29,7 @@ const FormElementSelect: React.FC<Props> = ({
   value,
   options,
   onChange,
-  className,
+  className = "",
   disabled,
   includeInfo,
   infoText,
@@ -42,18 +42,18 @@ const FormElementSelect: React.FC<Props> = ({
 }) => {
   const [showInfo, setShowInfo] = useState(false);
   const toggleInfo = () => {
-    setShowInfo(prevShowInfo => !prevShowInfo);
+    setShowInfo((prevShowInfo) => !prevShowInfo);
   };
   const parentRef = useRef<HTMLDivElement>(null);
   return (
     <div ref={parentRef} className={`flex flex-col items-start w-full ${className}`}>
       {/* {label && <span className="mr-5 w-[195px]">{label}</span>} */}
       <STIXPropertyLabel
-        propName={property ? property.name : label || ''}
+        propName={property ? property.name : label || ""}
         propertyType={property ? property.type : undefined}
         showTypeSelector={showTypeSelector}
         onTypeChange={onTypeChange}
-        additionalLabelClasses={'mr-2'}
+        additionalLabelClasses={"mr-2"}
       />
       <div className={`relative group flex items-center w-full`}>
         <select
@@ -69,35 +69,35 @@ const FormElementSelect: React.FC<Props> = ({
           dark:placeholder-neutralc-300 
           border
           border-neutralc-500
-          ${disabled ? 'cursor-not-allowed opacity-30' : undefined}
+          ${disabled ? "cursor-not-allowed opacity-30" : undefined}
           ${additionalClasses}
         `}
           disabled={disabled}
         >
-          {placeholder !== undefined ?
-            <option disabled>{placeholder}</option>
-            : null
-          }
+          {placeholder !== undefined ? <option disabled>{placeholder}</option> : null}
 
           {options.map((option, i) => {
-            return (
-              <option key={i}>{option}</option>
-            )
+            return <option key={i}>{option}</option>;
           })}
         </select>
         <InfoButton
           visible={includeInfo}
           toggleInfo={toggleInfo}
           additionalInfoClasses={`absolute top-0 right-0 ${additionalInfoClasses}`}
-          additionalStyle={{ transform: 'translate(50%, -50%)' }}
+          additionalStyle={{ transform: "translate(50%, -50%)" }}
           parentRef={parentRef}
         />
       </div>
-      {showInfo && includeInfo && infoText && infoText?.length > 0 &&
-        <AlertComponent alertText={infoText || ''} alertType={'info'} userClosable={false} className={'!mx-0 !my-1 !py-1 !px-2 text-xs'}></AlertComponent>
-      }
+      {showInfo && includeInfo && infoText && infoText?.length > 0 && (
+        <AlertComponent
+          alertText={infoText || ""}
+          alertType={"info"}
+          userClosable={false}
+          className={"!mx-0 !my-1 !py-1 !px-2 text-xs"}
+        ></AlertComponent>
+      )}
     </div>
   );
-}
+};
 
 export default FormElementSelect;
